bd734a7831dff3fe0d65751027bc2f70f252f8e8
Dan Pitic <dpitic@gmail.com>
2017-03-21 20:33:40 +0800

    Completed chapter 03. Up next: Chapter 04.

---
+++ ch03/types-test/app.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by dpitic on 21/03/17.
+ */
+
+var module = angular.module('module', []);
+module.controller('MainCtrl',
+    function MainCtrl($scope: angular.IScope) {
+        const set = new Set<any>();
+    });
